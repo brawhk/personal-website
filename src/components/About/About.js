79fdde3f20e8e84da9eee79051228dc6da1393b5
Brock Major <brockmajor@gmail.com>
2022-06-24 23:12:58 -0400

    finished projects and added contact + minor tweaks

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,12 @@
 import './About.css'
-import pic from '../../images/webpic.jpg'
+// import pic from '/images/webpic.jpg'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 function About() {
   return (
     <div className='about' id='about'>
-      <div className='header'>
+      <div className='header mb-4'>
         <h1>About Me</h1>
       </div>
       {/* <div className='body'>
@@ -20,14 +20,15 @@ function About() {
 
       <Row className="justify-content-md-center">
         <Col xs={12} md={6} lg={7}>
-          <p className='fs-6'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum, magna eu mollis commodo, ex velit condimentum nibh, et ornare elit magna in tellus. Sed vulputate nisi eros, in posuere elit ullamcorper et. Praesent consequat ante at elementum sollicitudin. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus est vel ante accumsan, in pulvinar elit fringilla. Curabitur faucibus tempor orci, sit amet tempor ligula varius nec. Nulla porttitor ac urna et efficitur. Nam et fringilla urna. Phasellus euismod, neque vel iaculis interdum, nunc turpis viverra massa, id sollicitudin lectus nisl at leo. Maecenas sit amet quam ut risus pharetra bibendum. Nunc a dapibus nisl, id elementum urna.
-
-            Quisque et dapibus mauris. Pellentesque nulla odio, consequat ac mollis eget, sodales eget nulla. Aliquam nunc magna, lobortis ut luctus vitae, hendrerit et arcu. Etiam commodo massa eget purus congue tincidunt. Proin nec turpis pellentesque, maximus leo in, tincidunt nulla. Sed blandit vitae est non porta. Vestibulum pretium nec urna vehicula blandit. In euismod venenatis mollis. In augue ligula, tempus non pellentesque quis, vestibulum et nisi.
+          <p>
+            Hello! My name is Brock and I am a Spring 2022 graduate from the University of Florida with a major in Computer Science and a minor in Business Administration. I began coding back in 2014, and have had a passion for it ever since!
+            <br/>
+            <br/>
+            I am currently searching for full time Software Engineering positions.
           </p>
         </Col>
         <Col xs={12} md={6} lg={5}>
-          <img className='mx-auto d-block pic' src={pic}/>
+          <img className='mx-auto d-block picture' src='/images/webpic.jpg'/>
         </Col>
       </Row>
     </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,38 @@
 import Item from './Item'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+// import logo from '../../images/fittrackpluslogo.png'
 import './Projects.css'
+
+const data = [
+  {
+    id: 0,
+    logoPath: '',
+    title: 'Personal Website',
+    link: 'https://github.com/brawhk/personal-website',
+    desc: 'View the source code for this website! Created using React'
+  },
+  {
+    id: 1,
+    logoPath: '/images/fittrackpluslogo.png',
+    title: 'FitTrack Plus',
+    link: 'https://github.com/brawhk/FitTrackPlus',
+    desc: `A social fitness tracking application for iOS developed using React Native. Log workout data and 
+    weight history with graphs to view progress over time. Log daily caloric intake using API to search for 
+    calorie info on select foods, or manually add meals. Join groups with friends to chat and view their progress`
+  },
+  {
+    id: 2,
+    logoPath: '/images/fittracklogo.png',
+    title: 'FitTrack',
+    link: 'https://github.com/brawhk/FitTrack',
+    desc: `A fitness tracking web app developed using Django. View suggested workouts to hit certain muscle
+    groups and add them to today's workout log. Log workout data and weight history with graphs to view progress over time.`
+  },
+  
+
+]
 
 function Projects() {
   return (
@@ -8,7 +41,12 @@ function Projects() {
         <h1>Projects</h1>
       </div>
       <div className='items'>
-        <Item/>
+        {/* <Item/> */}
+        <Row xs={1} md={2} className="g-4">
+          {Array.from({ length: data.length }).map((_, i) => (
+            <Item data={data[i]}/>
+          ))}
+        </Row>
       </div>
     </div>
   );
